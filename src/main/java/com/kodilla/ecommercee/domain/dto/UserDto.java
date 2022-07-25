353a1bccd4e6e0f93cd00399829321436e9889ec
Weronika Malicka <weronikamarta.malicka@gmail.com>
2022-07-25 17:57:29 +0200

    Corrected commit

--- src/main/java/com/kodilla/ecommercee/domain/dto/UserDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/dto/UserDto.java
@@ -1,2 +1,13 @@
-package com.kodilla.ecommercee.domain.dto;public class UserDto {
+package com.kodilla.ecommercee.domain.dto;
+
+public class UserDto {
+    private Long userId;
+    private String firstName;
+    private String lastName;
+
+    public UserDto(Long userId, String firstName, String lastName) {
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 }
