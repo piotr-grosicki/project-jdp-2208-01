b080e207955e5e21728a8df960de9b4fee8a6b3e
HubertJarzynka <hubjarz@gmail.com>
2022-07-25 17:40:14 +0200

    JDP220801-2 - fix

--- src/main/java/com/kodilla/ecommercee/controller/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/controller/GroupController.java
@@ -1,4 +1,31 @@
 package com.kodilla.ecommercee.controller;
+import com.kodilla.ecommercee.domain.dto.GroupDto;
 
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/group")
 public class GroupController {
+
+    @GetMapping
+    public List<GroupDto> getAllGroups() {
+        return new ArrayList<>();
+    }
+
+    @PostMapping
+    public GroupDto createGroup(@RequestBody GroupDto groupDto) {
+        return new GroupDto();
+    }
+
+    @GetMapping(value = "{id}")
+    public GroupDto getById(@PathVariable Long id) {
+        return new GroupDto();
+    }
+
+    @PutMapping
+    public void updateGroup(@RequestBody GroupDto groupDto) {
+
+    }
 }
