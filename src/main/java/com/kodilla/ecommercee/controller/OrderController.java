75d1353a67eadbc04eebd253c9d9c178e68a3aa2
Marcin Paszenda <marcinpaszenda@gmail.com>
2022-07-26 22:26:04 +0200

    Added OrderController

--- src/main/java/com/kodilla/ecommercee/controller/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/controller/OrderController.java
@@ -1,4 +1,37 @@
 package com.kodilla.ecommercee.controller;
 
+import com.kodilla.ecommercee.domain.dto.OrderDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/orders")
 public class OrderController {
+
+    @GetMapping
+    public List<OrderDto> getOrders() {
+        return new ArrayList<>();
+    }
+
+    @PostMapping
+    public OrderDto createOrder(@RequestBody OrderDto orderDto) {
+        return new OrderDto();
+    }
+
+    @GetMapping("/{id}")
+    public OrderDto getOrder(@PathVariable Long id) {
+        return new OrderDto();
+    }
+
+    @PutMapping
+    public void updateOrder(@RequestBody OrderDto orderDto) {
+
+    }
+
+    @DeleteMapping(value = "{id}")
+    public void deleteOrder(@PathVariable Long id) {
+
+    }
 }
