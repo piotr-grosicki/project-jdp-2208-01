f7926b43dfcab1315ca2f5018f4b6de281eabb64
Weronika Malicka <weronikamarta.malicka@gmail.com>
2022-07-26 10:24:51 +0200

    Corrected lombok annotations

--- src/main/java/com/kodilla/ecommercee/domain/dto/UserDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/dto/UserDto.java
@@ -1,13 +1,13 @@
 package com.kodilla.ecommercee.domain.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+@Getter
+@AllArgsConstructor
 public class UserDto {
+
     private Long userId;
     private String firstName;
     private String lastName;
 
-    public UserDto(Long userId, String firstName, String lastName) {
-        this.userId = userId;
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
 }
