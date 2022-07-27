4e56a7ecf680ba750a305caefe4f889cf4ae7ad0
Jakub Mihuka <jakubmihulka02@gmail.com>
2022-07-27 11:12:35 +0200

    Dodanie encji User

--- src/main/java/com/kodilla/ecommercee/domain/User.java
+++ src/main/java/com/kodilla/ecommercee/domain/User.java
@@ -1,19 +1,42 @@
 package com.kodilla.ecommercee.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
-@AllArgsConstructor
+@Getter
 @NoArgsConstructor
+@AllArgsConstructor
 @Entity(name = "users")
 public class User {
 
     @Id
-    @NotNull
     @GeneratedValue
     @Column(name = "user_id", unique = true)
     private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "status")
+    private Boolean content;
+
+    @Column(name = "userkey")
+    private Long userKey;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "cart_id", referencedColumnName = "cart_id")
+    private Cart cart;
+
+    @OneToMany(
+            targetEntity = Order.class,
+            mappedBy = "user",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.EAGER
+    )
+    private final List<Order> orders = new ArrayList<>();
 }
