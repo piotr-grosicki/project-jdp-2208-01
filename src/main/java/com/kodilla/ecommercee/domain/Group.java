742c3563b1c36f2cb1833f9c2d05c1adc4e127fe
Weronika Malicka <weronikamarta.malicka@gmail.com>
2022-07-27 13:31:36 +0200

    Added product entity with simple implementation of connected entities. Added allow public key retrieval

--- src/main/java/com/kodilla/ecommercee/domain/Group.java
+++ src/main/java/com/kodilla/ecommercee/domain/Group.java
@@ -3,17 +3,21 @@ package com.kodilla.ecommercee.domain;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity(name = "users")
-public class User {
+@Entity(name = "groups")
+public class Group {
 
     @Id
     @NotNull
     @GeneratedValue
-    @Column(name = "user_id", unique = true)
+    @Column(name = "group_id", unique = true)
     private Long id;
+
 }
--- src/main/java/com/kodilla/ecommercee/domain/Product.java
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,12 +23,29 @@ public class Product {
     @Column(name = "product_id", unique = true)
     private Long id;
 
-    @ManyToMany(cascade = CascadeType.ALL)
+    @ManyToOne
+    @JoinColumn(name = "group_id")
+    private Group group;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price")
+    private BigDecimal price;
+
+    @Column(name = "availability")
+    private boolean availability;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     @JoinTable(
             name = "products_orders",
             joinColumns = {@JoinColumn(name = "product_id", referencedColumnName = "product_id")},
             inverseJoinColumns = {@JoinColumn(name = "order_id", referencedColumnName = "order_id")}
     )
     private List<Order> orders = new ArrayList<>();
+
+    @ManyToMany(cascade = CascadeType.ALL, mappedBy = "products")
+    private List<Cart> carts = new ArrayList<>();
+
 
 }
