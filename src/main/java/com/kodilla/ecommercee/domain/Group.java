93307b4b5eff932798ab4c29e4b9a54774a219e8
HubertJarzynka <hubjarz@gmail.com>
2022-07-27 18:28:25 +0200

    JDP220801-13 dodanie relacji w Product, stworzyłem encje Group

---
+++ src/main/java/com/kodilla/ecommercee/domain/Group.java
@@ -0,0 +1,36 @@
+package com.kodilla.ecommercee.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "group_table")
+public class Group {
+
+    @Id
+    @NotNull
+    @GeneratedValue
+    @Column(name = "group_id", unique = true)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(
+            targetEntity = Product.class,
+            mappedBy = "group",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.EAGER
+    )
+    private final List<Product> products = new ArrayList<>();
+
+}
