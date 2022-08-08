29f42b1eada54e60eb97d059554e59b7bc78ac75
Marcin Paszenda <marcinpaszenda@gmail.com>
2022-08-08 23:44:42 +0200

    JDP220801-24 OrderController implementacja wstępna 2.0

--- src/main/java/com/kodilla/ecommercee/repository/OrderRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/OrderRepository.java
@@ -4,9 +4,7 @@ import com.kodilla.ecommercee.domain.Order;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.crypto.spec.OAEPParameterSpec;
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface OrderRepository extends CrudRepository<Order, Long> {
--- src/main/java/com/kodilla/ecommercee/service/DbOrderService.java
+++ src/main/java/com/kodilla/ecommercee/service/DbOrderService.java
@@ -2,19 +2,19 @@ package com.kodilla.ecommercee.service;
 
 import com.kodilla.ecommercee.domain.Order;
 import com.kodilla.ecommercee.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class DbOrderService {
 
+    @Autowired
     private final OrderRepository orderRepository;
-
-    DbOrderService(OrderRepository orderRepository) {
-        this.orderRepository = orderRepository;
-    }
 
     public Optional<Order> findOrderById(Long id) {
         return orderRepository.findById(id);
