73e1661ef2d7c02447dc2a2ed3fab865a478de6c
Jakub Mihuka <jakubmihulka02@gmail.com>
2022-08-08 22:11:01 +0200

    Change of user objects in UserController

--- src/main/java/com/kodilla/ecommercee/controller/UserController.java
+++ src/main/java/com/kodilla/ecommercee/controller/UserController.java
@@ -13,18 +13,18 @@ import java.util.List;
 public class UserController {
     @GetMapping()
     public List<UserDto> getUsers() {
-        return Arrays.asList(new UserDto(1L, "John", "Louis"), new UserDto(2L, "Patrick", "David"));
+        return Arrays.asList(new UserDto(1L, "marcin_kowalski2", true, 12345L), new UserDto(2L, "marcin_nowak", true, 12346L));
     }
     @GetMapping(value = "{userId}")
     public UserDto getUser(@PathVariable Long userId) {
-        return new UserDto(1L, "John", "Smith");
+        return new UserDto(1L, "marcin_kowalski2", true, 12345L);
     }
     @DeleteMapping(value = "{userId}")
     public void deleteUser(@PathVariable Long userId) {
     }
     @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public UserDto updateUser(@RequestBody UserDto userDto) {
-        return new UserDto(1L, "John", "Smith");
+        return new UserDto(1L, "marcin_kowalski2", true, 12345L);
     }
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public void createUser(@RequestBody UserDto userDto) {
