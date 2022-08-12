0d6d1c5078c6555f13588538814974715249f4a8
Kamil Kwieciński <100489738+Kamil-Kwiecinski@users.noreply.github.com>
2022-08-12 14:58:58 +0200

    Update GroupRepository.java

--- src/main/java/com/kodilla/ecommercee/repository/GroupRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/GroupRepository.java
@@ -6,7 +6,23 @@ import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 @Transactional
 public interface GroupRepository extends CrudRepository<Group, Long> {
+
+   @Override
+    List<Group> findAll();
+
+    @Override
+    Optional<Group> findById(Long groupId);
+
+    @Override
+    Group save(Group group);
+
+    @Override
+    void deleteById(Long groupId);
+    
 }
