df667cd71bf8ef0f39f50de5b023b06ded8ffe34
Tobiasz <tobijas.ch@gmail.com>
2022-08-16 10:09:39 +0200

    JDP220801-23pop5

--- src/main/java/com/kodilla/ecommercee/mapper/CartMapper.java
+++ src/main/java/com/kodilla/ecommercee/mapper/CartMapper.java
@@ -2,7 +2,6 @@ package com.kodilla.ecommercee.mapper;
 
 import com.kodilla.ecommercee.domain.Cart;
 import com.kodilla.ecommercee.domain.Product;
-import com.kodilla.ecommercee.domain.User;
 import com.kodilla.ecommercee.domain.dto.CartDto;
 import com.kodilla.ecommercee.repository.ProductRepository;
 import com.kodilla.ecommercee.repository.UserRepository;
